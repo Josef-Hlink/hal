0c9a4fd701f74eb1e3de106b37f07e6c84678d43
Josef Hamelink <91252200+Josef-Hlink@users.noreply.github.com>
2025-08-07 16:49:39 +0200

    my streams

--- hal/data/streams.py
+++ hal/data/streams.py
@@ -30,112 +30,19 @@ class StreamRegistry:
 
 # Only create streams if AWS_BUCKET is configured
 if AWS_BUCKET is not None:
-    RankedPlatinumStream = Stream(
-        remote=f"s3://{AWS_BUCKET}/hal/ranked/platinum",
-        local=f"{REPO_DIR}/data/ranked/platinum",
+    SampleFoxStream = Stream(
+        remote=f"s3://{AWS_BUCKET}/sample-fox",
+        local=f"{REPO_DIR}/data/sample-fox",
         proportion=1.0,
         keep_zip=True,
     )
 
-    RankedDiamondStream = Stream(
-        remote=f"s3://{AWS_BUCKET}/hal/ranked/diamond",
-        local=f"{REPO_DIR}/data/ranked/diamond",
+    FullStream = Stream(
+        remote=f"s3://{AWS_BUCKET}/full",
+        local=f"{REPO_DIR}/data/full",
         proportion=1.0,
         keep_zip=True,
     )
 
-    RankedMasterStream = Stream(
-        remote=f"s3://{AWS_BUCKET}/hal/ranked/master",
-        local=f"{REPO_DIR}/data/ranked/master",
-        proportion=1.0,
-        keep_zip=True,
-    )
-
-    StreamRegistry.register("ranked-platinum", RankedPlatinumStream)
-    StreamRegistry.register("ranked-diamond", RankedDiamondStream)
-    StreamRegistry.register("ranked-master", RankedMasterStream)
-
-
-### Top players
-
-# Only create streams if AWS_BUCKET is configured
-if AWS_BUCKET is not None:
-    AkloStream = Stream(
-        remote=f"s3://{AWS_BUCKET}/hal/top_players/Aklo",
-        local=f"{REPO_DIR}/data/top_players/Aklo",
-        proportion=1.0,
-        keep_zip=True,
-    )
-
-    AmsaStream = Stream(
-        remote=f"s3://{AWS_BUCKET}/hal/top_players/aMSa",
-        local=f"{REPO_DIR}/data/top_players/aMSa",
-        proportion=1.0,
-        keep_zip=True,
-    )
-
-    CodyStream = Stream(
-        remote=f"s3://{AWS_BUCKET}/hal/top_players/Cody",
-        local=f"{REPO_DIR}/data/top_players/Cody",
-        proportion=1.0,
-        keep_zip=True,
-    )
-
-    FranzStream = Stream(
-        remote=f"s3://{AWS_BUCKET}/hal/top_players/Franz",
-        local=f"{REPO_DIR}/data/top_players/Franz",
-        proportion=1.0,
-        keep_zip=True,
-    )
-
-    FrenzyStream = Stream(
-        remote=f"s3://{AWS_BUCKET}/hal/top_players/Frenzy",
-        local=f"{REPO_DIR}/data/top_players/Frenzy",
-        proportion=1.0,
-        keep_zip=True,
-    )
-
-    KodorinStream = Stream(
-        remote=f"s3://{AWS_BUCKET}/hal/top_players/Kodorin",
-        local=f"{REPO_DIR}/data/top_players/Kodorin",
-        proportion=1.0,
-        keep_zip=True,
-    )
-
-    Mang0Stream = Stream(
-        remote=f"s3://{AWS_BUCKET}/hal/top_players/mang0",
-        local=f"{REPO_DIR}/data/top_players/mang0",
-        proportion=2.0,
-        keep_zip=True,
-    )
-
-    MorsecodeStream = Stream(
-        remote=f"s3://{AWS_BUCKET}/hal/top_players/Morsecode",
-        local=f"{REPO_DIR}/data/top_players/Morsecode",
-        proportion=1.0,
-        keep_zip=True,
-    )
-
-    SFATStream = Stream(
-        remote=f"s3://{AWS_BUCKET}/hal/top_players/SFAT",
-        local=f"{REPO_DIR}/data/top_players/SFAT",
-        proportion=1.0,
-        keep_zip=True,
-    )
-
-    SolobattleStream = Stream(
-        remote=f"s3://{AWS_BUCKET}/hal/top_players/Solobattle",
-        local=f"{REPO_DIR}/data/top_players/Solobattle",
-        proportion=1.0,
-        keep_zip=True,
-    )
-
-    YCZStream = Stream(
-        remote=f"s3://{AWS_BUCKET}/hal/top_players/YCZ",
-        local=f"{REPO_DIR}/data/top_players/YCZ",
-        proportion=1.0,
-        keep_zip=True,
-    )
-
-    StreamRegistry.register("cody", CodyStream)
-    StreamRegistry.register("mang0", Mang0Stream)
+    StreamRegistry.register("sample-fox", SampleFoxStream)
+    StreamRegistry.register("full", FullStream)
