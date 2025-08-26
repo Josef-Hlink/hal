7702eab693d608761ed97ef43a8fdb63fcd56a92
Josef Hamelink <91252200+Josef-Hlink@users.noreply.github.com>
2025-08-26 14:50:59 +0200

    fix: local paths

--- hal/local_paths.py
+++ hal/local_paths.py
@@ -4,12 +4,13 @@ from typing import Final
 from pathlib import Path
 
 _ROOT = Path("/opt/slippi")
-_DATA = Path("~/Data/slippi").expanduser().resolve()
+_DATA = Path("~/Data").expanduser().resolve()
+_VAULT = _DATA / "Vault"
 
-_ISO_PATH = _ROOT / "Super Smash Bros. Melee (USA) (En,Ja) (v1.02).iso"
-_EMULATOR_PATH = _ROOT / "Slippi_Online-x86_64-ExiAI.AppImage"
-_REPLAYS_PATH = _DATA / "replays"
-_DOLPHIN_HOME_PATH = _ROOT / "Dolphin"
+_ISO_PATH = _VAULT / "Super Smash Bros. Melee (USA) (En,Ja) (v1.02).iso"
+_EMULATOR_PATH = _VAULT / "Slippi_Online-x86_64-ExiAI.AppImage"
+_REPLAYS_PATH = _VAULT / "replays"
+_DOLPHIN_HOME_PATH = _VAULT
 
 ISO_PATH: Final[str] = _ISO_PATH.as_posix()
 EMULATOR_PATH: Final[str] = _EMULATOR_PATH.as_posix()
@@ -17,4 +18,4 @@ REPLAYS_PATH: Final[str] = _REPLAYS_PATH.as_posix()
 DOLPHIN_HOME_PATH: Final[str] = _DOLPHIN_HOME_PATH.as_posix()
 
 EVAL_REPLAY_DIR: Final[str] = "/opt/projects/hal2/replays"
-REPO_DIR: Final[str] = "/opt/projects/hal2"
+REPO_DIR: Final[str] = "/home/jdham/Developer/own/hal"
